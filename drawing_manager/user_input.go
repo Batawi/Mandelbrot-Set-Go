2f1c9170f93f0eacb03d94624213ba874351f301
Michal Witwicki <michal.witwicki14@gmail.com>
2022-02-10 22:45:19 +0100

    Add moving fractal using mouse
    Signed-off-by: Michal Witwicki <michal.witwicki14@gmail.com>

--- drawing_manager/user_input.go
+++ drawing_manager/user_input.go
@@ -4,15 +4,33 @@ import (
 	"fmt"
 
 	"github.com/Batawi/Mandelbrot-Set-Go/fractal_manager"
+	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
+
+var mousePos1 pixel.Vec
+var mousePos2 pixel.Vec
 
 func checkUserInput(win *pixelgl.Window) {
 
 	if win.Pressed(pixelgl.KeyUp) {
 		fractal_manager.CameraMoveUp()
 	}
-	if win.Pressed(pixelgl.MouseButton1) {
-		fmt.Println("siema")
+	if win.Pressed(pixelgl.KeyDown) {
+		fractal_manager.CameraMoveDown()
 	}
+	if win.Pressed(pixelgl.KeyRight) {
+		fractal_manager.CameraMoveRight()
+	}
+	if win.Pressed(pixelgl.KeyLeft) {
+		fractal_manager.CameraMoveLeft()
+	}
+	if win.JustPressed(pixelgl.MouseButton1) {
+		mousePos1 = win.MousePosition()
+	} else if win.JustReleased(pixelgl.MouseButton1) {
+		mousePos2 = win.MousePosition()
+		fmt.Println(mousePos2.Sub(mousePos1))
+		fractal_manager.CameraMove(mousePos2.Sub(mousePos1))
+	}
+
 }
--- fractal_manager/fractal_manager.go
+++ fractal_manager/fractal_manager.go
@@ -1,6 +1,7 @@
 package fractal_manager
 
 import (
+	"fmt"
 	"math/cmplx"
 
 	"github.com/Batawi/Mandelbrot-Set-Go/utils"
@@ -9,59 +10,56 @@ import (
 )
 
 type Camera struct {
-	pos          pixel.Vec
-	moveSpeed    float64 // It is expressed as a percentage (from 0 to 1) of the window size
-	zoom         float64
-	camZoomSpeed float64
+	fractalBounds pixel.Rect // This is camera view boundries inside complex plane. Position vector is not needed
+	moveSpeed     float64    // It is expressed as a percentage (from 0 to 1) of the window size
+	zoom          float64
+	camZoomSpeed  float64
 }
 
 // --- GLOBALS ---
 var (
 	mainCam = Camera{
-		moveSpeed:    0.2,
-		zoom:         1,
-		camZoomSpeed: 10,
+		fractalBounds: pixel.R(-2, -2, 2, 2),
+		moveSpeed:     0.0001,
+		zoom:          1,
+		camZoomSpeed:  10,
 	}
 
-	canvas        *pixelgl.Canvas
-	deltaTime     *float64
-	windowBounds  pixel.Rect // This is size of application window
-	fractalBounds pixel.Rect // This is camera view boundries inside complex plane
+	canvas       *pixelgl.Canvas
+	deltaTime    *float64
+	windowBounds pixel.Rect // This is size of application window
+	// fractalResolution float64    = 0.1
 )
-
-func CameraMoveUp() {
-	mainCam.pos.Y += windowBounds.H() * mainCam.moveSpeed
-}
 
 func Init(bounds pixel.Rect, deltaTime *float64) *pixelgl.Canvas {
 	canvas = pixelgl.NewCanvas(bounds)
 	deltaTime = deltaTime
 	windowBounds = bounds
-	fractalBounds = pixel.R(-2, -2, 2, 2)
 
-	// Set fractal view bounds according to window bounds
-	fractalBounds = utils.ScaleRectToRect(windowBounds, fractalBounds)
+	// Set fractal view bounds according to application window bounds
+	mainCam.fractalBounds = utils.ScaleRectToRect(windowBounds, mainCam.fractalBounds)
+
+	// canvas.SetBounds(pixel.R(0, 0, canvas.Bounds().Max.X*fractalResolution, canvas.Bounds().Max.Y*fractalResolution))
 
 	return canvas
 }
 
 func Update() {
 
-	canvas.SetBounds(pixel.R(0, 0, 50, 50))
 	pixels := canvas.Pixels()
 
 	for i := 0; i < int(canvas.Bounds().H()); i++ {
 		for j := 0; j < int(canvas.Bounds().W()); j++ {
 
-			coord := complex(
-				utils.MapValueToRange(float64(j), 0, canvas.Bounds().W(), -2, 2),
-				utils.MapValueToRange(float64(i), 0, canvas.Bounds().H(), -2+mainCam.pos.Y, 2+mainCam.pos.Y))
+			point := complex(
+				utils.MapValueToRange(float64(j), 0, canvas.Bounds().W(), mainCam.fractalBounds.Min.X, mainCam.fractalBounds.Max.X),
+				utils.MapValueToRange(float64(i), 0, canvas.Bounds().H(), mainCam.fractalBounds.Min.Y, mainCam.fractalBounds.Max.Y))
 
-			escaping_point := coord
+			escapingPoint := point
 
 			c := 0
 			for c = 0; c < 20; c++ {
-				if cmplx.Abs(escaping_point) >= 4 { //4
+				if cmplx.Abs(escapingPoint) >= 4 { //4
 
 					pixels[i*4*int(canvas.Bounds().W())+j*4] = 5
 					pixels[i*4*int(canvas.Bounds().W())+j*4+1] = 5
@@ -69,8 +67,8 @@ func Update() {
 					pixels[i*4*int(canvas.Bounds().W())+j*4+3] = 255
 					break
 				}
-				// escaping_point = cmplx.Pow(escaping_point, complex(2, 0)) + coord
-				escaping_point = escaping_point*escaping_point + coord
+				// escapingPoint = cmplx.Pow(escapingPoint, complex(2, 0)) + point
+				escapingPoint = escapingPoint*escapingPoint + point
 			}
 
 			if c == 20 {
@@ -83,4 +81,30 @@ func Update() {
 		}
 	}
 	canvas.SetPixels(pixels)
+}
+
+func CameraMoveUp() {
+	mainCam.fractalBounds = mainCam.fractalBounds.Moved(pixel.V(0, windowBounds.H()*mainCam.moveSpeed))
+}
+
+func CameraMoveDown() {
+	mainCam.fractalBounds = mainCam.fractalBounds.Moved(pixel.V(0, windowBounds.H()*mainCam.moveSpeed*-1))
+}
+
+func CameraMoveRight() {
+	mainCam.fractalBounds = mainCam.fractalBounds.Moved(pixel.V(windowBounds.H()*mainCam.moveSpeed, 0))
+}
+
+func CameraMoveLeft() {
+	mainCam.fractalBounds = mainCam.fractalBounds.Moved(pixel.V(windowBounds.H()*mainCam.moveSpeed*-1, 0))
+}
+
+func CameraMove(v pixel.Vec) {
+	fmt.Println("before maping: ", v)
+
+	v.X = utils.MapValueToRange(v.X, 0, windowBounds.W(), 0, mainCam.fractalBounds.W())
+	v.Y = utils.MapValueToRange(v.Y, 0, windowBounds.H(), 0, mainCam.fractalBounds.H())
+
+	fmt.Println("after maping: ", v)
+	mainCam.fractalBounds = mainCam.fractalBounds.Moved(v)
 }
